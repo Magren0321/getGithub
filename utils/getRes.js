f72cca91643e35cd10b51c0c0dd3ed6939674b8b
Magren <44224328+Magren0321@users.noreply.github.com>
2021-06-01 19:47:50 +0800

    爬取项目地址，star数以及commit数

--- utils/getRes.js
+++ utils/getRes.js
@@ -2,8 +2,6 @@ import puppeteer from 'puppeteer';
 
 async function getData(name){
     const browser = await (puppeteer.launch({
-        //设置超时时间
-        timeout: 15000,
         //如果是访问https页面 此属性会忽略https错误
         ignoreHTTPSErrors: true,
         headless: true
@@ -31,11 +29,15 @@ async function getData(name){
             date: date
         });
     }
-    browser.close();
+   const repList = await toRepositories(name,browser)
+    // browser.close();
 
-    return dateList;
+    return {
+        dateList:dateList,
+        repList:repList
+    };
 }
-
+//获取日期以及当天的提交数
 async function getDateList(yearData,browser){
     const page = await browser.newPage();
     await page.goto(yearData);
@@ -56,8 +58,84 @@ async function getDateList(yearData,browser){
     return dateList;
 }
 
-async function getRepositories(url,browser){
+//获取所有项目的url
+async function toRepositories(name,browser){
+    const page = await browser.newPage();
+    await page.goto('https://github.com/'+name+'?tab=repositories');
+    let repositoriesUrl = [];
+    let nextUrl = undefined;
 
+    let rep = await getRep(page);
+
+    if(rep.urlList!=undefined){
+        for(let i of rep.urlList){
+            repositoriesUrl.push(i);
+        }
+    }
+
+    nextUrl = rep.nextUrl;
+
+    while(nextUrl!=undefined){
+        await page.goto(nextUrl);
+        rep = await getRep(page);
+        if(rep.urlList!=undefined){
+            for(let i of rep.urlList){
+                repositoriesUrl.push(i);
+            }
+        }
+
+        nextUrl = rep.nextUrl;
+    }
+    
+    let dataList = [];
+    for(let i of repositoriesUrl){
+        dataList.push(await getRepositoriesInfo(i,browser));
+    }
+
+    return dataList;
+
+}
+
+
+//获取当页的所有项目url
+async function getRep(page){
+        const rep =  await page.evaluate(()=>{
+            const url = document.querySelectorAll('.wb-break-all > a');
+            const next = document.querySelector('.BtnGroup > a');
+            let urlList = undefined;
+            let nextUrl = undefined;
+            if(url != null){
+                urlList = Array.prototype.map.call(url,(item)=>{
+                    return item.href;
+                })
+            }
+            if(next!=null&&next.outerText==='Next'){
+                nextUrl = next.href;
+            }
+            return {
+                urlList:urlList,
+                nextUrl:nextUrl
+            }
+        });
+        return rep;
+}
+//获取项目url，star数以及commit数
+async function getRepositoriesInfo(url,browser){
+    const page = await browser.newPage();
+    await page.goto(url);
+    const dataList = await page.evaluate(()=>{
+        const name = document.querySelectorAll('strong.mr-2.flex-self-stretch > a')[0].outerText;
+        const star = document.querySelectorAll('a.social-count.js-social-count')[0].getAttribute('aria-label').split(' ')[0];
+        const commit = document.querySelectorAll('span.d-none.d-sm-inline > strong')[0].outerText;
+
+        return data = {
+            name: name,
+            star: star,
+            commit :commit
+        }
+    })
+    dataList.url = url;
+    return dataList;
 }
 
 export default name =>{
